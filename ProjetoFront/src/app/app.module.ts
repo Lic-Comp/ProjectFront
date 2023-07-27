e895b0f4c81575662d020b4aef50561415484776
DâmarisCampos <camposdamaris08@gmail.com>
2023-07-27 20:23:43 -0300

    Create Component

--- ProjetoFront/src/app/app.module.ts
+++ ProjetoFront/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentoGridComponent } from './lancamento-grid/lancamento-grid.component';
+import { LancamentoPesquisaComponent } from './lancamento-pesquisa/lancamento-pesquisa.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { PessoaGridComponent } from './pessoa-grid/pessoa-grid.component';
+import { PessoaPesquisaComponent } from './pessoa-pesquisa/pessoa-pesquisa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentoCadastroComponent,
+    LancamentoGridComponent,
+    LancamentoPesquisaComponent,
+    NavBarComponent,
+    PessoaCadastroComponent,
+    PessoaGridComponent,
+    PessoaPesquisaComponent
   ],
   imports: [
     BrowserModule,
---
+++ ProjetoFront/src/app/pessoa-grid/pessoa-grid.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoa-grid',
+  templateUrl: './pessoa-grid.component.html',
+  styleUrls: ['./pessoa-grid.component.css']
+})
+export class PessoaGridComponent {
+
+}
